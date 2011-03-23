e74dca5a5622a88cdf5a2287cee3d5a7b894b02c
Ian Gustafson <iangustafson@gmail.com>
2011-03-23 01:43:49 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheBigBigBucks::Application.config.secret_token = 'afbd78990eafa3f1db2c0332cb0139a193ab7ad330bb9bcd84d64d1a576be8b7377dbe12e39c4eaaac7f1bc21962c5c4270861f2d456cdff38b50699eaa49503'
