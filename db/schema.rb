0445bdf5f1d2907349bd1148ecf07644c9da6979
Ian Gustafson <iangustafson@gmail.com>
2011-03-26 00:59:09 -0400

    Added clearance dependency
     * Added ckearace gem to Gemfile and rebundled
     * Ran the clearance install rake task to generate User model etc. & migrated
     * Added actionmailer config for site URL in emails
     * Added display of flash messages to layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110323063529) do
+ActiveRecord::Schema.define(:version => 20110326043655) do
 
   create_table "shows", :force => true do |t|
     t.datetime "scheduled_for"
@@ -25,5 +25,18 @@ ActiveRecord::Schema.define(:version => 20110323063529) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "encrypted_password", :limit => 128
+    t.string   "salt",               :limit => 128
+    t.string   "confirmation_token", :limit => 128
+    t.string   "remember_token",     :limit => 128
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email"
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
