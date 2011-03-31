b4b8d86ebc89f506c4eec0a3f1d79d4c9c7663ba
Ian Gustafson <iangustafson@gmail.com>
2011-03-31 01:22:06 -0400

    Added albums
     * Generated albums scaffold
     * Added generated scaffold stylesheet
     * Ran albums migration
     * Created error_messages partial (used in album form)

---
+++ app/models/album.rb
@@ -0,0 +1,4 @@
+class Album < ActiveRecord::Base
+  validates :title, :presence => true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110326043655) do
+ActiveRecord::Schema.define(:version => 20110329051848) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "title"
+    t.datetime "released_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "shows", :force => true do |t|
     t.datetime "scheduled_for"
