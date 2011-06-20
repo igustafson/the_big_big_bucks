16b165523fb604defedfdd3645f5734bee2c5550
Ian Gustafson <iangustafson@gmail.com>
2011-06-20 08:30:11 -0400

    Undid accidentally committed comment

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @shows = Show.all
-    @tweets = []#Tweet.find_all_for_user 'thebigbigbucks', :count => 6
+    @tweets = Tweet.find_all_for_user 'thebigbigbucks', :count => 6
     @blog_posts = BlogPost.all
     logger.debug @blog_posts
     render 'shared/home'
