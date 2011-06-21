bc21a69efb5a580789107ce40b0a19594ab5fbfb
Ian Gustafson <iangustafson@gmail.com>
2011-06-20 22:16:50 -0400

    Almost ready!
     * Added navigation bar mockup
     * ...a bunch of stuff

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,8 +2,7 @@ class HomeController < ApplicationController
   def index
     @shows = Show.all
     @tweets = Tweet.find_all_for_user 'thebigbigbucks', :count => 6
-    @blog_posts = BlogPost.all
-    logger.debug @blog_posts
+    @blog_posts = [BlogPost.first]
     render 'shared/home'
   end
 end
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,7 +1,17 @@
 class BlogPost
+  def self.source
+    'http://thebigbigbucks.wordpress.com/feed'
+  end
+
+  def self.feed
+    Feedzirra::Feed.fetch_and_parse source
+  end
+
+  def self.first
+    feed.entries.first
+  end
+
   def self.all
-    source = 'http://thebigbigbucks.wordpress.com/feed'
-    feed = Feedzirra::Feed.fetch_and_parse source
     feed.entries
   end
 
