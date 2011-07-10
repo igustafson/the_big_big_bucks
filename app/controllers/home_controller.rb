b6db2dc937eff769098cb16fc08c3ea5e1a0d993
Ian Gustafson <iangustafson@gmail.com>
2011-07-10 15:12:50 -0400

    Reworked tweets
     * Now using twitter js widgit
     * Removed tweet model
     * Styled twitter widget
     * Increased sidebat section padding
     * Increased navigation link size
     * Changed link styles all around
     * Widened sidebar to 220px

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,6 @@
 class HomeController < ApplicationController
   def index
     @shows = Show.all
-    @tweets = Tweet.find_all_for_user 'thebigbigbucks', :count => 6
     @blog_posts = [BlogPost.first]
     render 'shared/home'
   end
