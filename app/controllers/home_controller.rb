8c221f5c6288b7d9812b11c94eb9f03913b32322
Ian Gustafson <iangustafson@gmail.com>
2011-07-11 00:52:32 -0400

    * Now displays 3 blog posts  * Blog posts now have borders between them  * Tweaked margins  * Hit facebook and twitter links in blog posts

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @shows = Show.all
-    @blog_posts = [BlogPost.first]
+    @blog_posts = BlogPost.all[0..2]
     render 'shared/home'
   end
 end
